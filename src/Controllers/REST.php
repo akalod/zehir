0e7fbdfdf079914cb1d7d37f7b0a186e5f936cd1
Seyhan Yıldız <seyhan@digitalpanzehir.com>
2018-12-25 11:01:46 +0300

    -- get yok muymuş muş

--- src/Controllers/REST.php
+++ src/Controllers/REST.php
@@ -69,6 +69,9 @@ class REST
             case 'PATCH':
                 $this->PATCH();
                 break;
+            case 'GET':
+                $this->GET();
+                break;
             default:
         }
 
