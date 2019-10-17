1e30e62380014c7dc1955431571579136f5242a8
Seyhan Yıldız <seyhan@digitalpanzehir.com>
2019-10-17 14:29:20 +0300

    -- JSON builder a pointer ile eklemeceler

--- src/DB.php
+++ src/DB.php
@@ -8,6 +8,8 @@
 
 namespace Zehir\System;
 
+use Illuminate\Database\Query\Builder;
+
 class DB extends \Illuminate\Database\Capsule\Manager
 {
 
@@ -17,6 +19,52 @@ class DB extends \Illuminate\Database\Capsule\Manager
     {
         self::$totalQuery++;
         return parent::table($table, $connection);
+    }
+
+    /**
+     * @param Builder $db
+     * @param $column --nerede
+     * @param $object --objecnin ementi
+     * @param $cond -- koşul
+     * @param null $val --değer
+     * --- değer yoksa koşul eşit olup değer koşuldaki olarak çalışır (kısa kullanım)
+     * @return Builder
+     */
+    public static function jsonWhere(Builder &$db, $column, $object, $cond, $val = null)
+    {
+        if (!$val && $cond) {
+            $val = $cond;
+            $cond = '=';
+        }
+        if (!is_numeric($val)) {
+            $val = "'" . str_replace("'", "\'", $val) . "'";
+        }
+        return $db->whereRaw("json_extract($column, '$.$object') $cond $val");
+    }
+
+    /**
+     * karakter katarlarını temizleyerek geri döndürür
+     * JSON objectin sonucunu direk alabilmek için  $.test.name yerine
+     * test.name yapılabilir yazım methodu basitleştirmesi
+     * @param $column
+     * @param $select
+     * @return mixed
+     */
+    public static function jsonSelect($column,$select)
+    {
+        return DB::RAW('JSON_UNQUOTE(json_extract('.$column.',\'$.' . $select . '\'))');
+    }
+
+    public static function jsonWhereInArray(Builder &$db, $column, $array, $val)
+    {
+        if (!is_numeric($val)) {
+            $val = "'" . str_replace("'", "\'", $val) . "'";
+        }
+        return $db->whereRaw("
+        JSON_SEARCH(
+            json_extract($column, '$.$array')
+            ,'all',$val)
+        ");
     }
 
 }
